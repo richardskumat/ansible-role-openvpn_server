14d45734ffe252a865a92064ae976609a66a92d0
Richard Skumat <gitlab@richardskumat.com>
2019-07-29 09:56:35 +0100

    publish this role, however ugly it is.
    only works and tested on debian 9 kvm vm-s

---
+++ add_remotes_back.sh
@@ -0,0 +1,3 @@
+git remote add github git@github:richardskumat/ansible-role-openvpn_server
+git remote add gitlab git@gitlab:richardskumat/ansible-role-openvpn_server
+git remote add origin git@wsgitlab:qwe/ansible-role-openvpn_server
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Openvpn role
+
+Inspired by nyr's openvpn install script at https://github.com/Nyr/openvpn-install.
+
+It was fun writing this role, however it's not well tested and I wouldn't recommend
+anyone using it.
+
+
